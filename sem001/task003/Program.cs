eec0f260762729cc94e3373984c1a6c9434c1261
alexsavkaev <lesh1y174@gmail.com>
2023-01-26 21:23:10 +0300

    added task 3

---
+++ sem001/task003/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Введите первое число от 1 до 7: ");
+int day = int.Parse(Console.ReadLine());
+if(day == 1){
+    Console.Write("Понедельник");
+}
+if(day == 2){
+    Console.Write("Вторник");
+}
+if(day == 3){
+    Console.Write("Среда");
+}
+if(day == 4){
+    Console.Write("Четверг");
+}
+if(day == 5){
+    Console.Write("Пятница");
+}
+if(day == 6){
+    Console.Write("Суббота");
+}
+if(day == 7){
+    Console.Write("Воскресенье");
+}
