f95ce562262c760571f4a172b7d3ab332c987fc9
alexsavkaev <lesh1y174@gmail.com>
2023-01-26 21:10:39 +0300

    added tasks 1 and 2

---
+++ sem001/task001/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} Равен {sqr}");
---
+++ sem001/task002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
