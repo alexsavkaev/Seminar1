f464eafef976d15e4e5ac269469b705b66f020a8
alexsavkaev <lesh1y174@gmail.com>
2023-01-26 22:16:55 +0300

    added task 5

---
+++ sem001/task005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+// int number = int.Parse(Console.ReadLine());
+// if(number < 100 || number > 999){
+//     Console.WriteLine("Введено неправильное число");
+//     return;
+// }
+// int otvet = number % 10;
+// Console.WriteLine($"3 цифра: {otvet}");
+string number = Console.ReadLine();
+string otvet = Convert.ToString(number[2]);
+Console.WriteLine($"3 цифра: {otvet}");
