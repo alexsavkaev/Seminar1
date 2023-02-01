ac7df8d532f3d2aaa758c92356bbfc2b7cdb0b1e
alexsavkaev <lesh1y174@gmail.com>
2023-02-01 23:43:11 +0300

    added TryParse to task002

--- sem002/homework/task002/Program.cs
+++ sem002/homework/task002/Program.cs
@@ -9,14 +9,28 @@
 
 Console.Clear();
 Console.Write("Введите число: ");
-string num = Convert.ToString(Console.ReadLine());
-// Цикл while для того, чтобы программа не завершалась после ввода не подходящего числа
-while(num.Length < 3)
+string num = Console.ReadLine();
+
+long number1 = 0;
+bool numbersOnly = long.TryParse(num, out number1);  // Проверяем, чтобы введённые данные были именно числами, а не текстом
+if (numbersOnly == true)
+{
+    while(num.Length < 3)           // Цикл while для того, чтобы программа не завершалась после ввода не подходящего числа
 {
     Console.WriteLine("Нет третьей цифры, попробуйте еще раз: ");
     num = Convert.ToString(Console.ReadLine());
 }
 Console.Write($"{num} -> {num[2]}");
+}
+else
+Console.WriteLine("Введённые данные - не число");
+
+// while(num.Length < 3)
+// {
+//     Console.WriteLine("Нет третьей цифры, попробуйте еще раз: ");
+//     num = Convert.ToString(Console.ReadLine());
+// }
+// Console.Write($"{num} -> {num[2]}");
 
 // Решение через if
 // if (num.Length < 3)
