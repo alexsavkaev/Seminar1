0cab4a974a2db1743730e6fadf73563c17d5b4be
alexsavkaev <lesh1y174@gmail.com>
2023-02-02 22:10:32 +0300

    added task002

---
+++ sem003/task002/Program.cs
@@ -0,0 +1,55 @@
+﻿// **Задача 18:**
+
+// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+Console.Write("Введите Номер четверти: ");
+int quart = int.Parse(Console.ReadLine());
+
+
+// if(quart == 1)
+// {
+//     Console.WriteLine("x > 0; y > 0");
+// }else if(quart == 2)
+// {
+//     Console.WriteLine("x < 0; y > 0");
+// }else if(quart == 3)
+// {
+//     Console.WriteLine("x < 0; y < 0");
+// }else if(quart == 4)
+// {
+//     Console.WriteLine("x > 0; y < 0");
+// }else if (quart > 4 || quart < 1)
+// {
+//     Console.WriteLine("Это не номер четверти");
+// }
+
+switch (quart)
+{
+    case 1:
+    {
+        Console.WriteLine("x > 0; y > 0");
+       break; 
+    }
+    case 2:
+    {
+        Console.WriteLine("x < 0; y > 0");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("x < 0; y < 0");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("x > 0; y < 0");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Это не номер четверти");
+        break;
+    }
+}
