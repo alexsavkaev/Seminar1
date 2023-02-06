1dff863c406869f5fd7ffdbfd2227586a27ecb5e
alexsavkaev <lesh1y174@gmail.com>
2023-02-06 20:55:48 +0300

    added task 002

---
+++ sem004/task002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход 
+// число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+int Count(int num)
+{
+    int count = 0;
+    if (num < 0)
+        num *= (-1);
+    while (num > 0){
+        count++;
+        num /=10;
+    }
+    return count;
+    
+}
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write($"{Count(n)}");
+
