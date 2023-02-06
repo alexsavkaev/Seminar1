ee193c28fcf3bb85976b2710e7db5210758ba7b3
alexsavkaev <lesh1y174@gmail.com>
2023-02-06 21:49:48 +0300

    added task 004

---
+++ sem004/task004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит массив 
+// из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int[] array = GetBinArray(num);
+Console.Write($"[{String.Join(", ", array)}]");
+int[] GetBinArray(int size){
+    int[] result = new int[size];
+    for(int i = 0; i < size; i++){
+        result[i] = new Random().Next(2);
+    }
+    return result;
+}
