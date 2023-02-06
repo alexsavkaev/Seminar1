106b3192c20e65320d519c0a5830c3e19916f586
alexsavkaev <lesh1y174@gmail.com>
2023-02-06 20:35:18 +0300

    added task 001

---
+++ sem004/task001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число
+//  (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+int GetSum(int limit)
+{
+    int sum = 0;
+    for (int i = 1; i <= limit; i++)    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма от 1 до {n} равна {GetSum(n)}");
+
