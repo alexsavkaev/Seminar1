834ccf3a60d848f49c900f48f9a924f8d765ab38
alexsavkaev <lesh1y174@gmail.com>
2023-02-08 19:58:13 +0300

    added task 003

---
+++ sem004/homework/task003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.Write("Введите количество элементов в массиве: ");
+int num = int.Parse(Console.ReadLine()); 
+int[] array = GetRndArray(num);
+Console.Write($"[{String.Join(", ", array)}]");
+int[] GetRndArray(int size)
+{
+    int[] result = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(99);
+    }
+    return result;
+}
