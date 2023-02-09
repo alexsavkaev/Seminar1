bfdedc63cf8cfd700a1abebfb586e3ea138d5154
alexsavkaev <lesh1y174@gmail.com>
2023-02-09 20:56:45 +0300

    added task 001

---
+++ sem005/task001/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+
+
+Console.Clear();
+int Prompt (string message)
+{
+    Console.Write(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int[] GenerateArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue +1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.WriteLine("]");
+}
+int length = Prompt("Длина массива: ");
+int min = Prompt("Минимальное значение для диапазона случайного числа: ");
+int max = Prompt("Максимальное значение для диапазона случайного числа: ");
+int[] array = GenerateArray(length, min, max);
+PrintArray(array);
