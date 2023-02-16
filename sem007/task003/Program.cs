802b8a7035ea50f37bd7e301b8c1423d74ecdfed
alexsavkaev <lesh1y174@gmail.com>
2023-02-16 21:24:57 +0300

    added task 003

---
+++ sem007/task003/Program.cs
@@ -0,0 +1,57 @@
+﻿// **Задача 49:** Задайте двумерный массив.
+// Найдите элементы, у которых оба индекса чётные,
+// и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 2 3 4
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 **81** 2 **9**
+// 8 4 2 4
+// 1 **4** 3 **16**
+
+using System;
+using static System.Console;
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+int[,] IsElementEven(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if(i % 2 != 0 && j % 2 != 0)
+            {
+                inArray[i,j] *= inArray[i,j];
+            }
+        }
+    }
+    return inArray;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+int[,] mas = GetArray(5,5);
+PrintArray(mas);
+WriteLine();
+PrintArray(IsElementEven(mas));
