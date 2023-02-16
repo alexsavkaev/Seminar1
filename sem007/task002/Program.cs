f684607e3f499c95f7137609c1df1e1bce41c147
alexsavkaev <lesh1y174@gmail.com>
2023-02-16 20:44:04 +0300

    added task 002

---
+++ sem007/task002/Program.cs
@@ -0,0 +1,39 @@
+﻿// **Задача 48:** Задайте 
+// двумерный массив размера m на n, каждый элемент
+//  в массиве находится по формуле: A[i][j] = i + j. 
+//Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+
+// 0 1 2 3
+
+// 1 2 3 4
+
+// 2 3 4 5
+using System;
+using static System.Console;
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+int[,] mas = GetArray(3,4);
+PrintArray(mas);
